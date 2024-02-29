f826427853b3ed550313021bf59cbb5f1fdf3885
Julio Mateos Langerak <julio.matoes-langerak@igh.cnrs.fr>
2024-02-29 22:44:52 +0100

    Added hypothesis settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,6 +25,63 @@ from omero.plugins.user import UserControl
 from omero.rtypes import rint
 from pydantic.color import Color
 
+from hypothesis import HealthCheck, Verbosity, settings
+
+settings.register_profile(
+    "push",
+    max_examples=1,
+    suppress_health_check=[
+        HealthCheck.too_slow,
+        HealthCheck.large_base_example,
+        HealthCheck.data_too_large,
+    ],
+    deadline=100000,
+)
+settings.register_profile(
+    "pull_request",
+    max_examples=50,
+    suppress_health_check=[
+        HealthCheck.too_slow,
+        HealthCheck.large_base_example,
+        HealthCheck.data_too_large,
+    ],
+    deadline=100000,
+)
+settings.register_profile(
+    "pre_release",
+    max_examples=100,
+    suppress_health_check=[
+        HealthCheck.too_slow,
+        HealthCheck.large_base_example,
+        HealthCheck.data_too_large,
+    ],
+    deadline=100000,
+)
+settings.register_profile(
+    "dev",
+    max_examples=1,
+    suppress_health_check=[
+        HealthCheck.too_slow,
+        HealthCheck.large_base_example,
+        HealthCheck.data_too_large,
+    ],
+    deadline=100000,
+    verbosity=Verbosity.verbose,
+    print_blob=True,
+)
+settings.register_profile(
+    "debug",
+    max_examples=10,
+    suppress_health_check=[
+        HealthCheck.too_slow,
+        HealthCheck.large_base_example,
+        HealthCheck.data_too_large,
+    ],
+    deadline=100000,
+    verbosity=Verbosity.verbose,
+    print_blob=True,
+)
+
 # Settings for OMERO
 DEFAULT_OMERO_USER = "root"
 DEFAULT_OMERO_PASS = "omero"
